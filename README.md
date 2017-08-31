2ecf45ac2169dbab6fe31e179d571e3f6d1d7d14
Tony Hendrick <tony@staff.42.us.org>
2017-08-31 15:51:32 -0700

    First steps

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ you then update the saved decision.
 Each set of actions is an array of 4 different float values which relate to the joints
 of Marvin (Ex. [-0.01040708, -0.03782682, -0.02203254,  0.00013583]). There is a variety of different mathematical matrice equations catered to the evolution strategy to find different float values that may be best. You then go about getting one of those values and sending it
 as a action then comparing the reward value and saving it if the reward is better than the previous iterations reward.
+
+Still trying to figure out where exactly it checks to see if the current episode is better than the previous to learn...
+
+Looking at other solutions seems to be the best way to go. Reading the mathematical theories behind evolution is a great way to go about it as well such as [Evolution Strategies as a Scalabale Alternative to Reinforcement Learning](https://arxiv.org/pdf/1703.03864.pdf)
+
+# Current Progress
+
+My code is finally doing something interesting. Marvin is walking about as good a dog standing, actually that may be too much credit. I now just need to figure out how to keep him learning as he goes and make beter decisions rather than just grab random data from the time.
+
+![First Steps GIF](http://www.giphy.com/gifs/3o7aD5GTkoAoPvz3uU)
--- marvin.py
+++ marvin.py
@@ -1,185 +1,85 @@
-# import gym
-
-# # wrappers allows gym to record performance
-# from gym import wrappers
-
-# # Sets which environment to get
-# env = gym.make('Marvin-v0')
-
-# # Sets wrapper location
-# #env = wrappers.Monitor(env, '/tmp/marvin_exp1', force=True)
-
-# # Prints out available actions
-# print(env.action_space)
-
-# print (env.observation_space)
-
-# for i_episode in range(20):
-# 	observation = env.reset()
-# 	for i_episode in range(1000):
-
-# 		env.render()
-
-# 		# Starts the process
-# 		print (observation)
-
-# 		# Gets random action
-# 		action = env.action_space.sample()
-
-# 		# Starts action
-# 		observation, reward, done, info = env.step(action)
-
-# 		print ("Reward is %s" % (reward))
-
-# 		# Renders to screen
-# 		env.render()
-
-# 		# Checks if still alive if not starts next episode
-# 		if done:
-# 			print("Episode finished")
-# 			break
-
-
 ##############################
 
 	#My Current Try
 
 ##############################
 
-# import gym
-
-# class Marvin:
-# 	LEARNING_RATE = 0.01
-
-# 	def __init__(self):
-# 		self.env = gym.make('Marvin-v0')
-
-# 	def play(self, episodes, render=False):
-# 		for i_episode in range(episodes):
-# 			observation = self.env.reset()
-# 			while True:
-# 				# Starts the process
-# 				#print (observation)
-
-# 				# Gets random action
-# 				action = self.env.action_space.sample()
-
-# 				print (self.env.observation_space[0])
-
-# 				# Starts action
-# 				observation, reward, done, info = self.env.step(self.env.observation_space.low)
-
-# 				print ("Reward is %s" % (reward))
-
-# 				# Renders to screen
-# 				if render:
-# 					self.env.render()
-
-# 				# Checks if still alive if not starts next episode
-# 				if done:
-# 					print("Episode finished")
-# 					break
-
-# marvin = Marvin()
-# marvin.play(10, render=True)
-
-#############################
-
-	# Working Evolution strategy
-
-############################
-
-import random
-import cPickle as pickle
-import numpy as np
-from evostra import EvolutionStrategy
-from model import Model
 import gym
+import time
+import numpy as np
+
+class Marvin:
+	LEARNING_RATE = 0.01
+
+	def __init__(self):
+		self.env = gym.make('Marvin-v0')
+		self.model = Model()
+
+	def get_action(self, state):
+			hl = np.matmul(state, self.model.matrix['W1'])
+			hl = np.tanh(hl)
+			action = np.matmul(hl, self.model.matrix['W2'])
+			action = np.tanh(action)
+
+			return action
+
+	def start(self, episodes, render=False):
+		for i in range(episodes):
+			state = self.env.reset()
+			total_reward = 0
+			for j in range(10000):
+				if render:
+					self.env.render()
+				action = self.get_action(state)
+				state, reward, done, info = self.env.step(action)
+				total_reward += reward
+
+				print ("State: %s Reward: %s Done %s Info %s" % (state, reward, done, info))
+
+				if done:
+					break
+		return total_reward
 
 
-class Agent:
+	def play(self, episodes, render=False):
+		for i_episode in range(episodes):
+			observation = self.env.reset()
+			while True:
+				# Starts the process
+				#print (observation)
 
-    AGENT_HISTORY_LENGTH = 1
-    POPULATION_SIZE = 20
-    EPS_AVG = 1
-    SIGMA = 0.1
-    LEARNING_RATE = 0.01
-    INITIAL_EXPLORATION = 1.0
-    FINAL_EXPLORATION = 0.0
-    EXPLORATION_DEC_STEPS = 1000000
+				# Gets random action
+				action = self.env.action_space.sample()
 
-    def __init__(self):
-        self.env = gym.make('Marvin-v0')
-        self.model = Model()
-        self.es = EvolutionStrategy(self.model.get_weights(), self.get_reward, self.POPULATION_SIZE, self.SIGMA, self.LEARNING_RATE)
-        self.exploration = self.INITIAL_EXPLORATION
+				print (self.env.observation_space[0])
 
+				# Starts action
+				observation, reward, done, info = self.env.step(self.env.observation_space.low)
 
-    def get_predicted_action(self, sequence):
-        prediction = self.model.predict(np.array(sequence))
-        return prediction
+				print ("Reward is %s" % (reward))
 
+				# Renders to screen
+				if render:
+					self.env.render()
 
-    def load(self, filename='weights.pkl'):
-        with open(filename,'rb') as fp:
-            self.model.set_weights(pickle.load(fp))
-        self.es.weights = self.model.get_weights()
+				# Checks if still alive if not starts next episode
+				if done:
+					print("Episode finished")
+					break
 
+class Model:
 
-    def save(self, filename='weights.pkl'):
-        with open(filename, 'wb') as fp:
-            pickle.dump(self.es.get_weights(), fp)
+	def __init__(self):
+		hl_size = 100
+		t = int( time.time() * 1000.0 )
+		np.random.seed( ((t & 0xff000000) >> 24) +
+             			((t & 0x00ff0000) >>  8) +
+             			((t & 0x0000ff00) <<  8) +
+             			((t & 0x000000ff) << 24)   )
+		self.matrix = {}
+		self.matrix['W1'] = np.random.randn(24, hl_size) / np.sqrt(24)
+		self.matrix['W2'] = np.random.randn(hl_size, 4) / np.sqrt(hl_size)
 
-
-    def play(self, episodes, render=True):
-        self.model.set_weights(self.es.weights)
-        for episode in xrange(episodes):
-            total_reward = 0
-            observation = self.env.reset()
-            sequence = [observation]*self.AGENT_HISTORY_LENGTH
-            done = False
-            while not done:
-                if render:
-                    self.env.render()
-                action = self.get_predicted_action(sequence)
-                observation, reward, done, _ = self.env.step(action)
-                total_reward += reward
-                sequence = sequence[1:]
-                sequence.append(observation)
-            print "total reward:", total_reward
-
-
-    def train(self, iterations):
-        self.es.run(iterations, print_step=1)
-
-
-    def get_reward(self, weights):
-        total_reward = 0.0
-        self.model.set_weights(weights)
-
-        for episode in xrange(self.EPS_AVG):
-            observation = self.env.reset()
-            #print ("Observation %s" % (observation))
-            sequence = [observation]*self.AGENT_HISTORY_LENGTH
-            #print ("Sequence %s" % (sequence))
-            done = False
-            while not done:
-                self.exploration = max(self.FINAL_EXPLORATION, self.exploration - self.INITIAL_EXPLORATION/self.EXPLORATION_DEC_STEPS)
-                #print ("Self exploration %s" % (self.exploration))
-                if random.random() < self.exploration:
-                    action = self.env.action_space.sample()
-                else:
-                    action = self.get_predicted_action(sequence)
-                    #print ("Action predicted %s" % (action))
-                #print ("Action: %s" % (action))
-                observation, reward, done, _ = self.env.step([-0.00573673,  0.00121508,  0.01270417,  0.01607033])
-                total_reward += reward
-                sequence = sequence[1:]
-                sequence.append(observation)
-                #print ("WEIGHTS %s" % (self.model.weights))
-
-        return total_reward/self.EPS_AVG
-
-agent = Agent()
-agent.train(20)
-agent.play(10, render=True)
+marvin = Marvin()
+marvin.start(1000, render=True)
+#marvin.play(10, render=True)
